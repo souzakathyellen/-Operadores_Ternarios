123f0dac3a97298f4593fb9313fe40a3c19e3eaa
Kathyellen Souza <87395970+souzakathyellen@users.noreply.github.com>
2022-10-25 16:37:46 -0300

    Add files via upload

---
+++ OperadoresTernarios/OperadoresTernari0s/src/OperadoresTernarios.java
@@ -0,0 +1,11 @@
+public class OperadoresTernarios {
+    public static void main(String[] args) throws Exception {
+    
+    int n1, n2, r;
+    n1 = 4;
+    n2 = 8;
+    r = (n1 > n2)?0:1;
+    System.out.println(r);
+
+    }
+}
